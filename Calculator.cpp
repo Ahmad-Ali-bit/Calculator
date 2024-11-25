17e91b742f497a8e00b4025e7a7b5b5ab280262b
Ahmad-Ali-bit <m5626moham@gmail.com>
2024-11-25 14:57:02 +0500

    i have made comments on every step for easier understanding Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,22 +1,30 @@
-#include <iostream>
-using namespace std;
-int main()
+#include <iostream>                  // header file
+using namespace std;                 // so we dont have to use std::
+int main()                           //allocating main body
 {
- int a,b;
- char c;
- cout<<"add two numbers"<<endl;
- cin>>a>>b;
- cout<<"what function(+,-)"<<endl;
- cin>>c;
- switch (c)
+ int a,b;                            //declaring a,b
+ char c;                             //declearing c
+ cout<<"add two numbers"<<endl;      //asking value for two numbers
+ cin>>a>>b;                          //taking in answer
+ cout<<"what function(+,-)"<<endl;   //asking what program do you want to do
+ cin>>c;                             //taking in answer
+ switch (c)                          //taking switch case for different answer
  {
-   case (+);
-   {
+  case (+):                         // +case
+  {
      cout<<a+b;
-   }
-   case (-)
-   {
+  }
+  case (-):                         // -case
+  {
       cout<<a-b;
-   }
+  }
+  case (/):                        // /case
+  {
+      cout<<a/b;
+  }
+  case (*):
+  {
+       cout<<a*b;                  // *case
+  }
  }
 }
