57bb2b1e6987603c02c573eb39263a9621b0b272
Ahmad-Ali-bit <m5626moham@gmail.com>
2024-11-25 14:23:39 +0500

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ char c;
+ cout<<"add two numbers"<<endl;
+ cin>>a>>b;
+ cout<<"what function(+,-)"<<endl;
+ cin>>c;
+ switch (c)
+ {
+   case (+);
+   {
+     cout<<a+b;
+   }
+   case (-)
+   {
+      cout<<a-b;
+   }
+ }
+}
